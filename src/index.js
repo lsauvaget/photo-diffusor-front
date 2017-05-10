7d6e1abb5af8ba0d951833a9aa4dd974c2f26d88
Léo Sauvaget <yatmak@gmail.com>
2017-05-10 20:09:08 +0200

    feat(): add react router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import { render } from 'react-snapshot';
 import App from './containers/App.js'
 import { Provider } from 'react-redux';
 import store from './store';
+import { Router, Route} from 'react-router'
+import createHistory from 'history/createBrowserHistory'
 import './index.css';
+
+const history = createHistory();
 
 render(
     <Provider store={store}>
-        <App />
+        <Router history={history}>
+            <Route path="/" component={App} />
+        </Router>
     </Provider>,
   document.getElementById('root')
 );
